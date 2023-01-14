304b986fb90733a4cc030483687decfea886a13d
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2023-01-14 13:40:37 +0100

    Update Lock.js

--- test/Lock.js
+++ test/Lock.js
@@ -1,6 +1,6 @@
 const {
   time,
-  loadFixture,11
+  loadFixture,1
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
